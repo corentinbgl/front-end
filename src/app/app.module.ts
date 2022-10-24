02c0cc6f7bf2d26065fc8a05f8f7cf3ff6bbffdb
melina <meliina13.mp2@gmail.com>
2022-10-24 16:05:21 +0200

    [ADD] landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ComicsEdwardComponent } from './comics-edward/comics-edward.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComicsEdwardComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/comics-edward/comics-edward.component.ts
@@ -0,0 +1,10 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-comics-edward',
+  templateUrl: './comics-edward.component.html',
+  styleUrls: ['./comics-edward.component.scss']
+})
+export class ComicsEdwardComponent {
+  
+}
