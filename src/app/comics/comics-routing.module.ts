ba68659791cf810c956e43aee1ef3fb3c16d278c
corentin <cb.bagnol@gmail.com>
2022-11-02 13:45:24 +0100

    [ADD] carousel detail

--- src/app/comics/comics-routing.module.ts
+++ src/app/comics/comics-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetailcomicsComponent } from './components/detailcomics/detailcomics.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'detail/:comics_id', component : DetailcomicsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
