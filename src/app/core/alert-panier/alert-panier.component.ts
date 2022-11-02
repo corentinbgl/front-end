41fe198fd1563f042dca34c8b889e6cbee4f17f6
melina <meliina13.mp2@gmail.com>
2022-11-02 10:05:24 +0100

    [ADD] component alert-panier

---
+++ src/app/core/alert-panier/alert-panier.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-alert-panier',
+  templateUrl: './alert-panier.component.html',
+  styleUrls: ['./alert-panier.component.scss']
+})
+export class AlertPanierComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
